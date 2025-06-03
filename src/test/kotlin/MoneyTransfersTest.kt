a2f6f1e2a1e4837864fcee161936859cf835aa75
выавыавыаыв <wasiliy15959@gmail.com>
2025-06-03 11:07:02 +1000

    Fourth commit

--- src/test/kotlin/MoneyTransfersTest.kt
+++ src/test/kotlin/MoneyTransfersTest.kt
@@ -3,7 +3,7 @@ import org.junit.Test
 
 class MoneyTransfersTest {
     @Test
-    fun calculateNonCommission() {
+    fun calculateVKPayNonCommission() {
         //arrange
         val cardType: String = "VK Pay"
         val monthlyTransfers: Double = 0.0
@@ -16,16 +16,120 @@ class MoneyTransfersTest {
     }
 
     @Test
-    fun calculateAddCommission() {
+    fun calculateVKPayAddCommission() {
         //arrange
-        val cardType: String = "Mastercard"
-        val monthlyTransfers: Double = 500_000.0
-        val transferAmount: Double = 35_000.0
+        val cardType: String = "VK Pay"
+        val monthlyTransfers: Double = 30_000.0
+        val transferAmount: Double = 4_000.0
 
         //act
         val result = calculateCommission(cardType, monthlyTransfers, transferAmount)
 
         assertEquals(0.0, result, 0.0)
+    }
+
+    @Test
+    fun calculateMastercardNonCommission() {
+        //arrange
+        val cardType: String = "VK Pay"
+        val monthlyTransfers: Double = 0.0
+        val transferAmount: Double = 150.0
+
+        //act
+        val result = calculateCommission(cardType, monthlyTransfers, transferAmount)
+
+        assertEquals(/* expected = */ 0.0, /* actual = */ result, /* delta = */ 0.0)
+    }
+
+    @Test
+    fun calculateMastercardAddCommission() {
+        //arrange
+        val cardType: String = "VK Pay"
+        val monthlyTransfers: Double = 30_000.0
+        val transferAmount: Double = 4_000.0
+
+        //act
+        val result = calculateCommission(cardType, monthlyTransfers, transferAmount)
+
+        assertEquals(0.0, result, 0.0)
+    }
+
+    @Test
+    fun calculateVisaNonCommission() {
+        //arrange
+        val cardType: String = "VK Pay"
+        val monthlyTransfers: Double = 0.0
+        val transferAmount: Double = 150.0
+
+        //act
+        val result = calculateCommission(cardType, monthlyTransfers, transferAmount)
+
+        assertEquals(/* expected = */ 0.0, /* actual = */ result, /* delta = */ 0.0)
+    }
+
+    @Test
+    fun calculateVisaAddCommission() {
+        //arrange
+        val cardType: String = "VK Pay"
+        val monthlyTransfers: Double = 0.0
+        val transferAmount: Double = 150.0
+
+        //act
+        val result = calculateCommission(cardType, monthlyTransfers, transferAmount)
+
+        assertEquals(/* expected = */ 0.0, /* actual = */ result, /* delta = */ 0.0)
+    }
+
+    @Test
+    fun calculateVKPayDailyLimitCommission() {
+        //arrange
+        val cardType: String = "VK Pay"
+        val monthlyTransfers: Double = 0.0
+        val transferAmount: Double = 150.0
+
+        //act
+        val result = calculateCommission(cardType, monthlyTransfers, transferAmount)
+
+        assertEquals(/* expected = */ 0.0, /* actual = */ result, /* delta = */ 0.0)
+    }
+
+    @Test
+    fun calculateVKPayMonthlyLimitCommission() {
+        //arrange
+        val cardType: String = "VK Pay"
+        val monthlyTransfers: Double = 0.0
+        val transferAmount: Double = 150.0
+
+        //act
+        val result = calculateCommission(cardType, monthlyTransfers, transferAmount)
+
+        assertEquals(/* expected = */ 0.0, /* actual = */ result, /* delta = */ 0.0)
+    }
+
+    @Test
+    fun calculateCardDailyLimitCommission() {
+        //arrange
+        val cardType: String = "VK Pay"
+        val monthlyTransfers: Double = 0.0
+        val transferAmount: Double = 150.0
+
+        //act
+        val result = calculateCommission(cardType, monthlyTransfers, transferAmount)
+
+        assertEquals(/* expected = */ 0.0, /* actual = */ result, /* delta = */ 0.0)
+    }
+
+    @Test
+    fun calculateCardMonthlyLimitCommission() {
+        //arrange
+        val cardType: String = "VK Pay"
+        val monthlyTransfers: Double = 0.0
+        val transferAmount: Double = 150.0
+
+        //act
+        val result = calculateCommission(cardType, monthlyTransfers, transferAmount)
+
+        assertEquals(/* expected = */ 0.0, /* actual = */ result, /* delta = */ 0.0)
     }
 
 }
